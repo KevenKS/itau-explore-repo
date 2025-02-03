074f75d6779f435f2b78de75d5ef04ce3bae41ed
Keven Silva <keven.sp@hotmail.com>
2025-02-03 17:24:47 -0300

    Criação componente e router PR

---
+++ src/app/components/pull-requests/pull-requests.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pull-requests',
+  imports: [],
+  templateUrl: './pull-requests.component.html',
+  styleUrl: './pull-requests.component.scss'
+})
+export class PullRequestsComponent {
+
+}
