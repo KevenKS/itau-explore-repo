c67d1de88d6039483afdf9f350aec88dd9e3cc79
Keven Silva <keven.sp@hotmail.com>
2025-02-03 17:34:19 -0300

    Criação component docs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { RepoSearchComponent } from './components/repo-search/repo-search.component';
 import { PullRequestsComponent } from './components/pull-requests/pull-requests.component';
+import { DocumentationComponent } from './components/documentation/documentation.component';
 
 
 export const routes: Routes = [
     { path: '', component: RepoSearchComponent },
-    { path: 'pulls/:fullName', component: PullRequestsComponent }
+    { path: 'pulls/:fullName', component: PullRequestsComponent },
+    { path: 'docs', component: DocumentationComponent },
+    { path: '**', redirectTo: '' }
 ];
