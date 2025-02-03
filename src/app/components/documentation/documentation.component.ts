1caa3b776bf9031273f91cdfee7c27121c085c18
Keven Silva <keven.sp@hotmail.com>
2025-02-03 17:38:24 -0300

    logica component docs

--- src/app/components/documentation/documentation.component.ts
+++ src/app/components/documentation/documentation.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-documentation',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule, FontAwesomeModule],
   templateUrl: './documentation.component.html',
-  styleUrl: './documentation.component.scss'
+  styleUrls: ['./documentation.component.scss']
 })
 export class DocumentationComponent {
+  faArrowLeft = faArrowLeft;
 
+  constructor(private router: Router) {}
+
+  navigateHome() {
+    this.router.navigate(['/']);
+  }
 }
