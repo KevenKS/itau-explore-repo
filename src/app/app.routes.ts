3209998afad255cca7191ee674be85b682e152af
Keven Silva <keven.sp@hotmail.com>
2025-02-03 17:24:47 -0300

    Criação componente e router PR

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
-import { RepoSearchComponent } from './repo-search/repo-search.component';
+import { RepoSearchComponent } from './components/repo-search/repo-search.component';
+import { PullRequestsComponent } from './components/pull-requests/pull-requests.component';
 
 
 export const routes: Routes = [
-    { path: '', component: RepoSearchComponent }
+    { path: '', component: RepoSearchComponent },
+    { path: 'pulls/:fullName', component: PullRequestsComponent }
 ];
