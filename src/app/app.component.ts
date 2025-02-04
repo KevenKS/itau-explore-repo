4a4f021df10b0230bed082fbbedec2800ea66bc2
Keven Silva <keven.sp@hotmail.com>
2025-02-03 21:11:12 -0300

    refresh no icon-itau

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { SearchStateService } from './services/search-state.service';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,15 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private searchStateService: SearchStateService) {}
+
+  onLogoClick() {
+    this.searchStateService.setState({
+      searchTerm: '',
+      repositories: [],
+      totalCount: 0,
+      currentPage: 0
+    });
+  }
+}
